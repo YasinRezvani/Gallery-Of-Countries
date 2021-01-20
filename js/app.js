9b5834a4828fb7e0b0e752668e956374e45cfd0c
YasinRezvani <yasinrezvani2025@gmail.com>
2021-01-21 00:09:26 +0330

    new

--- js/app.js
+++ js/app.js
@@ -7,7 +7,24 @@ let image = document.querySelectorAll('.image');
             if (selected.target.classList.contains('item')){
                 items.querySelector('.all').classList.remove('all');
                 selected.target.classList.add('all');
+                let filtername = selected.target.getAttribute("data-name");
+                image.forEach((img) => {
+                    let filterimage = img.getAttribute("data-name");
+                    
+
+                    if(filtername == filterimage || filtername == "all"){
+                    img.classList.add("show");
+                }
+                else{
+                    img.classList.add("hide");
+                    img.classList.remove("show");
+                    
+                }
+                
+
+                });
+                
+                
             }
         }
     }
-
