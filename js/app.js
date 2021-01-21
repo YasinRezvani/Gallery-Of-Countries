1959e4dede530b1fe34de1951c58f72657fad7a5
YasinRezvani <yasinrezvani2025@gmail.com>
2021-01-21 03:57:00 +0330

    new

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
 let items = document.querySelector('.items');
 let image = document.querySelectorAll('.image');
 
+window.onload = () => {
+    items.onclick = (selected) => {
+        if (selected.target.classList.contains('item')) {
+            items.querySelector('.all').classList.remove('all');
+            selected.target.classList.add('all');
 
-    window.onload = () =>{
-        items.onclick= (selected) =>{
-            if (selected.target.classList.contains('item')){
-                items.querySelector('.all').classList.remove('all');
-                selected.target.classList.add('all');
-                let filtername = selected.target.getAttribute("data-name");
-                image.forEach((img) => {
-                    let filterimage = img.getAttribute("data-name");
-                    
+            let filtername = selected.target.getAttribute("data-name");
 
-                    if((filtername == filterimage) || filtername == "all"){
+            image.forEach((img) => {
+                let filterimage = img.getAttribute("data-name");
+
+                if ((filtername == filterimage) || filtername == "all") {
                     img.classList.remove("hide");
                     img.classList.add("show");
-                }
-                else{
+                } else {
                     img.classList.add("hide");
-                    img.classList.remove("show");                   
+                    img.classList.remove("show");
                 }
-                
-
-                });
-                
-                
-            }
+            });
         }
     }
+
+    for (let i = 0; i < image.length; i++) {
+        image[i].setAttribute('onclick', 'pre(this)');
+    }
+}
+
+const previewbox = document.querySelector('.preview-box');
+const icon = document.querySelector(".icon");
+
+    function pre(element){
+        previewbox.classList.add("show");
+        icon.onclick= () =>{
+            previewbox.classList.remove("show");
+        }
+    }
+
+
+
